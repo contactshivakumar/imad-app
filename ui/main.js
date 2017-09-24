6a23cfc24609ca361f170860215b260b371ed220
contactshivakumar <contactshivakumar@gmail.com>
2017-09-24 11:46:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 var counter=0;
 alert(button);
 
 button.onclick=function() {
 counter=counter+1;
 alert(counter.toString());
-var span=document.getElementById("count");
+var span=document.getElementById('count');
 span.innerHTML=counter.toString();
 };
